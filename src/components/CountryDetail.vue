3570564e575a772179b15563547100fa69d9df0d
Stan Tarnev <stan.tarnev@gmail.com>
2019-10-10 02:16:29 +0100

    some extensions

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -1,26 +1,31 @@
 <template lang="html">
   <div id="info-box" v-if='country'>
     <h3>{{country.name}}</h3>
+    <img class="flag" :src='country.flag'/>
     <p>Capital: {{country.capital}}</p>
     <p>Population: {{country.population}}</p>
+    <!-- <p v-for="language in country.languages">Languages: {{countries.languages.name}}</p> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'country-detail',
-  props: ['country']
+  props: ['country', 'countries']
 }
 </script>
 
 <style lang="css" scoped>
  #info-box {
    border: solid 2px black;
-   border-radius: 5px;
+   border-radius: 10px;
    padding: 10px;
    position: fixed;
    top: 100px;
    right: 100px;
-
  }
+
+.flag {
+  width: 120px;
+}
 </style>
